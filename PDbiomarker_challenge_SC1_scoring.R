107319accbe5f44303e5e74511faab4b05b15f91
sieberts <solly.sieberts@sagebase.org>
2017-06-30 15:15:37 -0700

    Updating caret Fit to quiet nnet output.

--- PDbiomarker_challenge_SC1_scoring.R
+++ PDbiomarker_challenge_SC1_scoring.R
@@ -16,10 +16,14 @@ library(data.table)
 
 synapseLogin()
 
-# replace rest and walk features here
-# leave walkmetadata and restmetadata as is
+args <- commandArgs(trailingOnly = TRUE)
+featsynid <- args[1]  # assumes all individuals (training and test) and all features (walk and rest) in single tsv file at this synId
+testset <- tolower(args[2]) # options are leaderboard, final or none
 
-trainingfeatureids<-c(rest="syn9952416", walk="syn9920447", walkmetadata="syn10138914", restmetadata="syn10142011")
+
+
+#trainingfeatureids<-c(rest="syn9952416", walk="syn9920447", walkmetadata="syn10138914", restmetadata="syn10142011")
+trainingfeatureids<-c(features=featsynid, walkmetadata="syn10138914", restmetadata="syn10142011")
 traininghcids<-"syn9819504"
 
 demotrainquery<-"SELECT * FROM syn9819507"
@@ -37,9 +41,10 @@ bad.records.rest<-NULL
 ############
 
 # Prepare Data
-walkvariables<-names(trainingfeatures$walk)
-walkvariables<-walkvariables[!tolower(walkvariables) %in% c("recordid","healthcode")]
-trainingwalk<-merge.data.frame(trainingfeatures$walkmetadata, trainingfeatures$walk, by=c("healthCode","recordId"), all.x=T, sort=F)
+walkvariables<-names(trainingfeatures$features)
+walkvariables<-walkvariables[grep("walk", walkvariables)]
+#walkvariables<-walkvariables[!tolower(walkvariables) %in% c("recordid","healthcode")]
+trainingwalk<-merge.data.frame(trainingfeatures$walkmetadata, trainingfeatures$features[,c("healthCode", "recordId", walkvariables)], by=c("healthCode","recordId"), all.x=T, sort=F)
 trainingwalk<-merge.data.frame(trainingdemo, trainingwalk, by=c("healthCode"), suffixes=c(".demo", ".walktest"), all.y=T, sort=F)
 trainingwalk<-trainingwalk[trainingwalk$error=="",]   #Comment out for now
 
@@ -108,7 +113,7 @@ greedy_ensemble_nophoneapp <- caretEnsemble(
     summaryFunction=twoClassSummary,
     classProbs=TRUE
   ))
-summary(greedy_ensemble_nophoneapp)
+#summary(greedy_ensemble_nophoneapp)
 
 
 ###########
@@ -116,9 +121,9 @@ summary(greedy_ensemble_nophoneapp)
 ############
 
 # Prepare Data
-restvariables<-names(trainingfeatures$rest)
-restvariables<-restvariables[!tolower(restvariables) %in% c("recordid","healthcode")]
-trainingrest<-merge.data.frame(trainingfeatures$restmetadata, trainingfeatures$rest, by=c("healthCode","recordId"), all.x=T, sort=F)
+restvariables<-names(trainingfeatures$features)
+restvariables<-restvariables[grep("rest", restvariables)]
+trainingrest<-merge.data.frame(trainingfeatures$restmetadata, trainingfeatures$features[,c("healthCode", "recordId", restvariables)], by=c("healthCode","recordId"), all.x=T, sort=F)
 trainingrest<-merge.data.frame(trainingdemo, trainingrest, by=c("healthCode"), suffixes=c(".demo", ".resttest"), all.y=T, sort=F)
 trainingrest<-trainingrest[trainingrest$error=="None",]   #Comment out for now
 
@@ -197,92 +202,105 @@ summary(greedy_ensemble_rest_nophoneapp)
 ##
 #############################
 
-demorestrictedquery<-"SELECT * FROM syn9819508"
-demorestrictedqueryResult <- synTableQuery(demorestrictedquery)
+if(testset!="none"){
+  if(testset=="leaderboard"){
+    demorestrictedquery<-"SELECT * FROM syn9819508"
+    demorestrictedqueryResult <- synTableQuery(demorestrictedquery)
 
-# replace rest and walk features here
-# leave walkmetadata and restmetadata as is
+    testfeatureids<-c(walkmetadata="syn10138365", restmetadata="syn10140543")
+    testfeatures = lapply(testfeatureids, downloadFile)
+    testdemo<-demorestrictedqueryResult@values
+  } else if(testset=="final"){
+    #broken for now
+  }
 
-testfeatureids<-c(rest="syn9955511", walk="syn9926958", walkmetadata="syn10138365", restmetadata="syn10140543")
-testfeatures = lapply(testfeatureids, downloadFile)
-testdemo<-demorestrictedqueryResult@values
+  ###########
+  # Walking test
+  ############
 
-###########
-# Walking test
-############
+  # Prepare Data
+  testwalk<-merge.data.frame(testfeatures$walkmetadata, testfeatures$features[,c("healthCode", "recordId", walkvariables)], by=c("healthCode","recordId"), all.x=T, sort=F)
+  testwalk<-merge.data.frame(testdemo, testwalk, by=c("healthCode"), suffixes=c(".demo", ".walktest"), all.y=T, sort=F)
+  testwalk<-testwalk[testwalk$error=="",]
+  test_hcs_inconsistent<-unique(testwalk$healthCode[testwalk$`professional-diagnosis`==FALSE&testwalk$medTimepoint%in%c("Immediately before Parkinson medication", "Just after Parkinson medication (at your best)")])
+  test_hcs_inconsistent<-test_hcs_inconsistent[!is.na(test_hcs_inconsistent)]
+  testwalk<-testwalk[!testwalk$healthCode %in% test_hcs_inconsistent,]
 
-# Prepare Data
-testwalk<-merge.data.frame(testfeatures$walkmetadata, testfeatures$walk, by=c("healthCode","recordId"), all.x=T, sort=F)
-testwalk<-merge.data.frame(testdemo, testwalk, by=c("healthCode"), suffixes=c(".demo", ".walktest"), all.y=T, sort=F)
-testwalk<-testwalk[testwalk$error=="",]
-test_hcs_inconsistent<-unique(testwalk$healthCode[testwalk$`professional-diagnosis`==FALSE&testwalk$medTimepoint%in%c("Immediately before Parkinson medication", "Just after Parkinson medication (at your best)")])
-test_hcs_inconsistent<-test_hcs_inconsistent[!is.na(test_hcs_inconsistent)]
-testwalk<-testwalk[!testwalk$healthCode %in% test_hcs_inconsistent,]
+  # Summarized by Median
 
-# Summarized by Median
+  dttestwalk<-data.table(testwalk)
+  mediantestwalk<-dttestwalk[, lapply(.SD, median, na.rm=TRUE), by=groupvariables, .SDcols=walkvariables ]
+  tmpcount<-dttestwalk[, .(.N), by = groupvariables]
+  mediantestwalk<-merge.data.frame(mediantestwalk, tmpcount, by=groupvariables)
 
-dttestwalk<-data.table(testwalk)
-mediantestwalk<-dttestwalk[, lapply(.SD, median, na.rm=TRUE), by=groupvariables, .SDcols=walkvariables ]
-tmpcount<-dttestwalk[, .(.N), by = groupvariables]
-mediantestwalk<-merge.data.frame(mediantestwalk, tmpcount, by=groupvariables)
+  testsubsetwalk<-mediantestwalk[mediantestwalk$age>=57&mediantestwalk$medTimepoint!="Just after Parkinson medication (at your best)"&mediantestwalk$medTimepoint!=""&mediantestwalk$medTimepoint!="Another time",]
+  testsubsetwalk<-testsubsetwalk[!is.na(testsubsetwalk$`professional-diagnosis`),]
+  testoutcome<-testsubsetwalk$`professional-diagnosis`
+  testoutcome<-factor(ifelse(testoutcome, "PD", "Control"))
+  testoutcome<-factor(testoutcome)
 
-testsubsetwalk<-mediantestwalk[mediantestwalk$age>=57&mediantestwalk$medTimepoint!="Just after Parkinson medication (at your best)"&mediantestwalk$medTimepoint!=""&mediantestwalk$medTimepoint!="Another time",]
-testsubsetwalk<-testsubsetwalk[!is.na(testsubsetwalk$`professional-diagnosis`),]
-testoutcome<-testsubsetwalk$`professional-diagnosis`
-testoutcome<-factor(ifelse(testoutcome, "PD", "Control"))
-testoutcome<-factor(testoutcome)
+  testfeatureswalk_nophoneapp<-cbind(testsubsetwalk[,c("age", "gender")], testsubsetwalk[,walkvariables])
+  testfeatureswalk_nophoneapp<-testfeatureswalk_nophoneapp[,!names(testfeatureswalk_nophoneapp)%in%c("steps_time_in_seconds", "numberOfSteps", "distance")]
+  testfeatureswalk_nophoneapp[,"gender"]<-as.factor(testfeatureswalk_nophoneapp[,"gender"])
 
-testfeatureswalk_nophoneapp<-cbind(testsubsetwalk[,c("age", "gender")], testsubsetwalk[,walkvariables])
-testfeatureswalk_nophoneapp<-testfeatureswalk_nophoneapp[,!names(testfeatureswalk_nophoneapp)%in%c("steps_time_in_seconds", "numberOfSteps", "distance")]
-testfeatureswalk_nophoneapp[,"gender"]<-as.factor(testfeatureswalk_nophoneapp[,"gender"])
+  tmptestmmatrix_nophoneapp<-model.matrix( ~ ., testfeatureswalk_nophoneapp)
+  tmptestmmatrix_nophoneapp<-tmptestmmatrix_nophoneapp[,-1]
 
-tmptestmmatrix_nophoneapp<-model.matrix( ~ ., testfeatureswalk_nophoneapp)
-tmptestmmatrix_nophoneapp<-tmptestmmatrix_nophoneapp[,-1]
 
+
+  ###########
+  # Standing test
+  ############
+
+  # Prepare Data
+  testrest<-merge.data.frame(testfeatures$restmetadata, testfeatures$features[,c("healthCode", "recordId", restvariables)], by=c("healthCode","recordId"), all.x=T, sort=F)
+  testrest<-merge.data.frame(testdemo, testrest, by=c("healthCode"), suffixes=c(".demo", ".resttest"), all.y=T, sort=F)
+  testrest<-testrest[testrest$error=="None",]
+  test_hcs_inconsistent_rest<-unique(testrest$healthCode[testrest$`professional-diagnosis`==FALSE&testrest$medTimepoint%in%c("Immediately before Parkinson medication", "Just after Parkinson medication (at your best)")])
+  test_hcs_inconsistent_rest<-test_hcs_inconsistent_rest[!is.na(test_hcs_inconsistent_rest)]
+  testrest<-testrest[!testrest$healthCode %in% test_hcs_inconsistent_rest,]
+
+  # Summarized by Median
+  dttestrest<-data.table(testrest)
+  mediantestrest<-dttestrest[, lapply(.SD, median, na.rm=TRUE), by=groupvariables.rest, .SDcols=restvariables ]
+  tmpcount<-dttestrest[, .(.N), by = groupvariables.rest]
+  mediantestrest<-merge.data.frame(mediantestrest, tmpcount, by=groupvariables.rest)
+
+
+  #
+  # Without App and Phone info
+  # Apply machine learning excluding medicated timepoints
+  #
+  testsubsetrest<-mediantestrest[mediantestrest$age>=57&mediantestrest$medTimepoint!="Just after Parkinson medication (at your best)"&mediantestrest$medTimepoint!=""&mediantestrest$medTimepoint!="Another time",]
+  testsubsetrest<-testsubsetrest[!is.na(testsubsetrest$`professional-diagnosis`),]
+  testoutcome_rest<-testsubsetrest$`professional-diagnosis`
+  testoutcome_rest<-factor(ifelse(testoutcome_rest, "PD", "Control"))
+  testoutcome_rest<-factor(testoutcome_rest)
+
+  testfeaturesrest_nophoneapp<-cbind(testsubsetrest[,c("age", "gender")], testsubsetrest[,restvariables])
+  testfeaturesrest_nophoneapp[,"gender"]<-as.factor(testfeaturesrest_nophoneapp[,"gender"])
+
+  tmptestmmatrix_rest_nophoneapp<-model.matrix( ~ ., testfeaturesrest_nophoneapp)
+  tmptestmmatrix_rest_nophoneapp<-tmptestmmatrix_rest_nophoneapp[,-1]
+
+} else {
+  testsubsetwalk<-trainsubsetwalk
+  testsubsetrest<-trainsubsetrest
+  testoutcome<-trainoutcome
+  testoutcome_rest<-testoutcome
+  tmptestmmatrix_nophoneapp <-tmpmmatrix_nophoneapp
+  tmptestmmatrix_rest_nophoneapp <- tmpmmatrix_rest_nophoneapp
+}
+  
 preds_allmodels_nophoneapp <- as.data.frame(predict(object=model_list_nophoneapp, tmptestmmatrix_nophoneapp))
 preds_greedensemble_nophoneapp <- predict(object=greedy_ensemble_nophoneapp, tmptestmmatrix_nophoneapp, type="prob")
 preds_allmodels_nophoneapp$ensemble<-preds_greedensemble_nophoneapp
 auc_allmodels_nophoneapp <- apply(preds_allmodels_nophoneapp, 2, function(x, testoutcome) {
   return(roc(ifelse(testoutcome=="PD",1,0), x)$auc)
 }, testoutcome=testoutcome)
-print(auc_allmodels_nophoneapp)
+#  print(auc_allmodels_nophoneapp)
 
 
-
-###########
-# Standing test
-############
-
-# Prepare Data
-testrest<-merge.data.frame(testfeatures$restmetadata, testfeatures$rest, by=c("healthCode","recordId"), all.x=T, sort=F)
-testrest<-merge.data.frame(testdemo, testrest, by=c("healthCode"), suffixes=c(".demo", ".resttest"), all.y=T, sort=F)
-testrest<-testrest[testrest$error=="None",]
-test_hcs_inconsistent_rest<-unique(testrest$healthCode[testrest$`professional-diagnosis`==FALSE&testrest$medTimepoint%in%c("Immediately before Parkinson medication", "Just after Parkinson medication (at your best)")])
-test_hcs_inconsistent_rest<-test_hcs_inconsistent_rest[!is.na(test_hcs_inconsistent_rest)]
-testrest<-testrest[!testrest$healthCode %in% test_hcs_inconsistent_rest,]
-
-# Summarized by Median
-dttestrest<-data.table(testrest)
-mediantestrest<-dttestrest[, lapply(.SD, median, na.rm=TRUE), by=groupvariables.rest, .SDcols=restvariables ]
-tmpcount<-dttestrest[, .(.N), by = groupvariables.rest]
-mediantestrest<-merge.data.frame(mediantestrest, tmpcount, by=groupvariables.rest)
-
-
-#
-# Without App and Phone info
-# Apply machine learning excluding medicated timepoints
-#
-testsubsetrest<-mediantestrest[mediantestrest$age>=57&mediantestrest$medTimepoint!="Just after Parkinson medication (at your best)"&mediantestrest$medTimepoint!=""&mediantestrest$medTimepoint!="Another time",]
-testsubsetrest<-testsubsetrest[!is.na(testsubsetrest$`professional-diagnosis`),]
-testoutcome_rest<-testsubsetrest$`professional-diagnosis`
-testoutcome_rest<-factor(ifelse(testoutcome_rest, "PD", "Control"))
-testoutcome_rest<-factor(testoutcome_rest)
-
-testfeaturesrest_nophoneapp<-cbind(testsubsetrest[,c("age", "gender")], testsubsetrest[,restvariables])
-testfeaturesrest_nophoneapp[,"gender"]<-as.factor(testfeaturesrest_nophoneapp[,"gender"])
-
-tmptestmmatrix_rest_nophoneapp<-model.matrix( ~ ., testfeaturesrest_nophoneapp)
-tmptestmmatrix_rest_nophoneapp<-tmptestmmatrix_rest_nophoneapp[,-1]
 
 preds_allmodels_rest_nophoneapp <- as.data.frame(predict(object=model_list_rest_nophoneapp, tmptestmmatrix_rest_nophoneapp))
 preds_greedensemble_rest_nophoneapp <- predict(object=greedy_ensemble_rest_nophoneapp, tmptestmmatrix_rest_nophoneapp, type="prob")
@@ -290,4 +308,12 @@ preds_allmodels_rest_nophoneapp$ensemble<-preds_greedensemble_rest_nophoneapp
 auc_allmodels_rest_nophoneapp <- apply(preds_allmodels_rest_nophoneapp, 2, function(x, testoutcome) {
   return(roc(ifelse(testoutcome=="PD",1,0), x)$auc)
 }, testoutcome=testoutcome_rest)
-print(auc_allmodels_rest_nophoneapp) 
+#  print(auc_allmodels_rest_nophoneapp) 
+
+out_walk<-data.frame(testsubsetwalk[,c("healthCode", "medTimepoint")], walk.predictions=preds_allmodels_nophoneapp$ensemble, stringsAsFactors = FALSE)
+out_rest<-data.frame(testsubsetrest[,c("healthCode", "medTimepoint")], rest.predictions=preds_allmodels_rest_nophoneapp$ensemble, stringsAsFactors = FALSE)
+
+write.table(merge.data.frame(out_walk, out_rest, by=c("healthCode", "medTimepoint"), all=T, sort = FALSE), paste(featsynid, "_", testset, ".txt"), row.names=F, quote=F)
+
+scores<-c(walk=auc_allmodels_nophoneapp$ensemble, rest=auc_allmodels_rest_nophoneapp$ensemble)
+cat(scores)
